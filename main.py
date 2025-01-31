e7b19776c8b8d15f403d410abd7d0301eb1c2f64
Adit <vyas.swati28@gmail.com>
2025-01-31 23:16:19 +0530

    Day 25: Written data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import glob as gb
 from fpdf import FPDF
+from pathlib import Path
 
 filepaths = gb.glob("Invoice/*.xlsx")
 
@@ -8,7 +9,9 @@ for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
     pdf = FPDF(orientation="P",unit="mm", format="A4")
     pdf.add_page()
-    Invoice_no = filepath[8:13]
+    filename = Path(filepath).stem
+    Invoice_no = filename.split("-")[0]
     print(Invoice_no)
     pdf.set_font(family="Times", style="B", size=18)
     pdf.cell(w=50, h=8, txt=f"Invoice no: {Invoice_no}")
+    pdf.output(f"PDFs/{filename}.pdf")
